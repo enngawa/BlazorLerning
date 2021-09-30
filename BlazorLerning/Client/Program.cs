3e2c3fba07b75e27faf55db7f0e74242f189bba8
enngawa <enngawas@gmail.com>
2021-09-30 21:47:30 +0900

    2章

--- BlazorLerning/Client/Program.cs
+++ BlazorLerning/Client/Program.cs
@@ -9,12 +9,16 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+using Syncfusion.Blazor; //<=追加
+
 namespace BlazorLerning.Client
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("ライセンスキー");//<=追加
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
@@ -25,7 +29,8 @@ namespace BlazorLerning.Client
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorLerning.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
-
+            
+            builder.Services.AddSyncfusionBlazor(); //<=追加
             await builder.Build().RunAsync();
         }
     }
